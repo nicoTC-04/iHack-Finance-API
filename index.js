04bca3484326540b0cf93e4c2c6db43ef9ea0671
nicoTC-04 <nicolastrevinoc@hotmail.com>
2024-03-02 23:16:33 -0600

    m

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const uploadsDirectory = '../iHack-Finance-API/tempFile/';
+const uploadsDirectory = 'tempFile/';
 
 // multer setup
 // Set up storage location and filenames for uploaded files
